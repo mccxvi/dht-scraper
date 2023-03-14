d358868628fe8c8301f8b2d78d332c9ddabfe8be
Mikołaj Połonowicz <65428411+mccxvi@users.noreply.github.com>
2023-03-14 20:37:22 +0100

    V1

---
+++ dht-scraper.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#/////////////////////////////////////////
+
+# 🠺 21/02/2023 21:31:35
+#
+# ↳ Author 🡢 Mikołaj Połonowicz @ MCCXVI
+# ↳ https://github.com/mccxvi
+
+#/////////////////////////////////////////
+
+import shutil
+import os
+import requests
+import mimetypes
+import json
+import re
+import secrets
+
+serverFile = input(" * | DHT File location: ")
+
+with open(serverFile, encoding="utf-8") as rd:
+	data = json.load(rd)
+	dbContent = data['data']
+	chnContent = data['meta']['channels']
+	findServerName = data['meta']['servers']
+
+for i in findServerName:
+	serverName = i['name']
+
+if not os.path.exists(f'dl/{serverName}'):
+    os.makedirs(f'dl/{serverName}')
+
+channelsFriendlyName = {}
+
+for i in chnContent:
+	channelsFriendlyName[f'{i}'] = (f'{chnContent[i]["name"]}')
+
+for cid in dbContent:
+	for (post, inside) in dbContent[cid].items():
+
+		if "a" in inside:
+			for cont in inside['a']:
+				url = cont['url']
+				response = requests.get(url)
+				content_type = response.headers['content-type']
+				ext = mimetypes.guess_extension(content_type)
+				if ext == None:
+					print(f" - | File doesn't exist anymore [Skipping]")
+				else:
+					fname = secrets.token_urlsafe(12)
+
+					for fcn in channelsFriendlyName:
+						if cid == fcn:
+							properChannelName = channelsFriendlyName[fcn]
+
+							if not os.path.exists(f'dl/{serverName}/{properChannelName}'):
+							    os.makedirs(f'dl/{serverName}/{properChannelName}')
+
+							if not os.path.exists(f'dl/{serverName}/{properChannelName}/{ext}'):
+								os.makedirs(f'dl/{serverName}/{properChannelName}/{ext}')
+
+							with open(f"dl/{serverName}/{properChannelName}/{ext}/{fname}{ext}", "wb") as f:
+								f.write(response.content)
+
+							print(f" + | Downloaded '{fname}{ext}' from [{properChannelName}]")			
+		else:
+			pass
+
+print(f"\n * | FINISHED DOWNLOADING FROM '{serverName}' SERVER")
